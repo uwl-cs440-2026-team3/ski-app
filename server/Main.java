2cd9bea4a53a6b05a5185a01d57735d7f2af9466
Josiah VanderZee <jvanderzee@apache.org>
2026-02-27 11:25:27 -0600

    Latch main thread until shutdown
    This is to ensure that the main thread does not exit and unexpectedly
    terminate the process.

--- server/Main.java
+++ server/Main.java
@@ -2,6 +2,7 @@ import com.sun.net.httpserver.*;
 import java.io.IOException;
 import java.net.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.CountDownLatch;
 import javax.net.ssl.SSLContext;
 
 public class Main {
@@ -25,10 +26,20 @@ public class Main {
         HttpsServer server = Main.getLocalServer();
         server.setHttpsConfigurator(new HttpsConfigurator(ctx));
         server.start();
+
+        // The Java 17 documentation does not seem to guarantee that the
+        // server thread will keep the process alive. It seems to do so
+        // on at least one system, but to be on the safe side, we latch
+        // the main thread until the server shuts down to ensure the
+        // process does not end prematurely.
+        CountDownLatch shutdownLatch = new CountDownLatch(1);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down...");
             server.stop(1);
+            shutdownLatch.countDown();
         }));
+
+        Main.waitUntilShutdown(shutdownLatch);
     }
 
     private static HttpsServer getLocalServer() throws UnknownHostException,
@@ -36,5 +47,17 @@ public class Main {
         InetAddress hostAddr = InetAddress.getByAddress(new byte[] {0, 0, 0, 0});
         return HttpsServer.create(new InetSocketAddress(hostAddr, Main.port),
                                   Main.connectionBacklog);
+    }
+
+    private static void waitUntilShutdown(CountDownLatch shutdownLatch) {
+        // The loop is necessary to make sure we do not terminate due to a
+        // supurious wake-up from a thread interrupt.
+        while (true) {
+            try {
+                shutdownLatch.await();
+                break;
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
