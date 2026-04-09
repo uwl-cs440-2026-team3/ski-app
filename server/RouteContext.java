f27d3eba4a1edb6ea48b9716ca5d8c1ad3569f3e
JosiahWI <41302989+JosiahWI@users.noreply.github.com>
2026-04-08 21:04:48 -0500

    Add endpoint to get list of races (#26)
    * Document /getraces endpoint
    
    * Implement /getraces according to docs
    
    ---------
    
    Co-authored-by: Mistsofnowh3r3 <legobrennan999@gmail.com>

--- server/RouteContext.java
+++ server/RouteContext.java
@@ -21,6 +21,12 @@ public class RouteContext {
                                   String course,
                                   String start,
                                   String duration) {};
+    public record RaceInfo(String name,
+                           String teamA,
+                           String teamB,
+                           String course,
+                           String start,
+                           String end) {};
     public record NoBodyRequest() {};
 
     public static void registerRoutes(HttpsServer server) {
@@ -45,6 +51,9 @@ public class RouteContext {
         server.createContext("/getmembers",
                              (HttpExchange hx) ->
                              new GetMembersHandler(hx).handle());
+        server.createContext("/getraces",
+                             (HttpExchange hx) ->
+                             new GetRacesHandler(hx).handle());
     }
 
     private static class TeamCreateHandler extends
@@ -352,5 +361,51 @@ public class RouteContext {
 
         private record MemberInfo(String email, String name, String role,
                                   String team) {}
+    }
+    private static class GetRacesHandler extends
+        AuthFlow.PrivilegedHandler<NoBodyRequest> {
+        public GetRacesHandler(HttpExchange hx) {
+            super(hx, NoBodyRequest.class, "GET");
+        }
+
+        @Override
+        void handleDetail(NoBodyRequest req) throws IOException {
+
+            try (Connection conn = DriverManager.getConnection(Config.databaseURL)) {
+                String sql = """
+                             SELECT r.name,
+                             ta.name AS team_a_name,
+                             tb.name AS team_b_name,
+                             c.name AS course_name,
+                             r.starttime AS start,
+                             r.endtime AS end
+                             FROM races r
+                             JOIN teams ta ON r.team_id_a = ta.teamid
+                JOIN teams tb ON r.team_id_b = tb.teamid
+                JOIN courses c ON r.course_id = c.courseid
+                                                ORDER BY datetime(r.starttime)
+                                                """;
+
+                ArrayList<RaceInfo> races = new ArrayList<>();
+
+                try (PreparedStatement ps = conn.prepareStatement(sql);
+                            ResultSet rs = ps.executeQuery()) {
+
+                    while (rs.next()) {
+                        races.add(new RaceInfo(rs.getString("name"),
+                                               rs.getString("team_a_name"),
+                                               rs.getString("team_b_name"),
+                                               rs.getString("course_name"),
+                                               rs.getString("start"),
+                                               rs.getString("end")));
+                    }
+                }
+
+                String response = RouteContext.JSONMapper.writeValueAsString(races);
+                this.sendText(200, response);
+            } catch (SQLException se) {
+                this.sendText(500, se.getMessage());
+            }
+        }
     }
 }
